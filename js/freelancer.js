9c4e045ec612b17b7d97149eb12df82f66a0e6c3
Deeksha Bajaj <dishab16@gmail.com>
2017-09-28 11:40:23 +0900

    add new content and change design

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,17 +1,11 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 //displaying stars
 $.fn.stars = function() {
     return $(this).each(function() {
         var rating = $(this).data("rating");
         var numStars = $(this).data("numStars");
-        var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>');
+        var fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-color fa-star"></i>');
         var halfStar = ((rating%1) !== 0) ? '<i class="fa fa-star-half-empty"></i>': '';
-        var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star-o"></i>');
+        var noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-uncolor fa-star"></i>');
         $(this).html(fullStar + halfStar + noStar);
     });
 }
